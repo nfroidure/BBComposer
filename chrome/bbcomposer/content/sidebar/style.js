420566db103364e852c6dfb78e3c24ea87777a43
Nicolas Froidure <nicolas_froidure@elitwork.com>
2012-02-09 22:22:26 +0100

    Refactoring of the styles sidebar

--- chrome/bbcomposer/content/sidebar/style.js
+++ chrome/bbcomposer/content/sidebar/style.js
@@ -1,54 +1,126 @@
-// INITIALIzATION
-function openPropertyDialog(file, value)
+function StyleManager()
+	{
+	this.loadHandler=ewkLib.newEventHandler(this,this.load);
+	window.addEventListener('load', this.loadHandler, false);
+	};
+
+StyleManager.prototype.load = function ()
+	{
+	document.removeEventListener('load', this.loadHandler, false);
+	if(window.parent.myBBComposerManager.toggleSidebar('css', true))
+		{
+		this.unLoadHandler=ewkLib.newEventHandler(this,this.unLoad);
+		document.addEventListener('unload', this.unLoadHandler, false);
+		this.displayHandler=ewkLib.newEventHandler(this,this.display);
+		document.addEventListener('display', this.displayHandler, false);
+		this.lockOnMenu=document.getElementById('bbcomposer-lockon-menu');
+		this.displayHandler=ewkLib.newEventHandler(this,this.display);
+		this.lockOnMenu.addEventListener('command', this.displayHandler, false);
+		document.getElementById('bbcomposer-cancel-button').addEventListener('command', this.displayHandler, false);
+		this.applyHandler=ewkLib.newEventHandler(this,this.apply);
+		document.getElementById('bbcomposer-apply-button').addEventListener('command', this.applyHandler, false);
+		this.resetHandler=ewkLib.newEventHandler(this,this.reset);
+		document.getElementById('bbcomposer-reset-button').addEventListener('command', this.resetHandler, false);
+		this.setPropertyHandler=ewkLib.newEventHandler(this,this.setProperty);
+		document.getElementById('font-family-button').addEventListener('command', this.setPropertyHandler, false);
+		document.getElementById('font-size-button').addEventListener('command', this.setPropertyHandler, false);
+		document.getElementById('background-image-button').addEventListener('command', this.setPropertyHandler, false);
+		document.getElementById('background-position-button').addEventListener('command', this.setPropertyHandler, false);
+		this.setSizeHandler=ewkLib.newEventHandler(this,this.setSize);
+		document.getElementById('border-width-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('line-height-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('text-indent-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('word-spacing-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('letter-spacing-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('text-shadow-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('padding-top-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('padding-right-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('padding-bottom-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('padding-left-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('margin-top-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('margin-right-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('margin-bottom-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('margin-left-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('width-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('height-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('top-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('right-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('bottom-button').addEventListener('command', this.setSizeHandler, false);
+		document.getElementById('left-button').addEventListener('command', this.setSizeHandler, false);
+		this.setSizesHandler=ewkLib.newEventHandler(this,this.setSizes);
+		document.getElementById('padding-button').addEventListener('command', this.setSizesHandler, false);
+		document.getElementById('margin-button').addEventListener('command', this.setSizesHandler, false);
+		this.display();
+		}
+	}
+
+StyleManager.prototype.display = function ()
+	{
+	this.getElementStyle(this.lockOnMenu.value);
+	}
+
+StyleManager.prototype.apply = function ()
+	{
+	window.parent.myBBComposerManager.focusedBBComposer.setElementStyle(this.lockOnMenu.value);
+	}
+
+StyleManager.prototype.reset = function ()
+	{
+	window.parent.myBBComposerManager.focusedBBComposer.resetElementStyle(this.lockOnMenu.value);
+	}
+
+StyleManager.prototype.getElementStyle = function (type)
+	{
+	window.parent.myBBComposerManager.focusedBBComposer.getElementStyle(type);
+	}
+
+StyleManager.prototype.openPropertyDialog = function (file, value)
 	{
 	var params = {inn:{property:value}, out:null};
 	window.openDialog(file, "", "chrome, dialog, modal, resizable=no", params).focus();
 	if(params.out&&params.out.property)
-		{
-		return params.out.property;
-		}
+	{
+	return params.out.property;
+	}
 	else
-		return false;
-	}
-function setProperty(propertyName)
-	{
-	var value = openPropertyDialog("chrome://bbcomposer/content/style/"+propertyName+".xul", document.getElementById(propertyName).value);
-	if(value)
-		document.getElementById(propertyName).value = value;
-	}
-function setSize(propertyName)
-	{
-	var value = openPropertyDialog("chrome://bbcomposer/content/style/size.xul", document.getElementById(propertyName).value)
-	if(value)
-		document.getElementById(propertyName).value = value;
+	return false;
 	}
 
-function setAllSizes(size_type)
+StyleManager.prototype.setProperty = function (event)
 	{
-	var size = openPropertyDialog("chrome://bbcomposer/content/style/size.xul", "");
+	var property=event.target.getAttribute('id').replace('-button','');
+	var element=document.getElementById(property);
+	var value = this.openPropertyDialog("chrome://bbcomposer/content/style/"+property+".xul", element.value);
+	if(value)
+		element.value = value;
+	}
+
+StyleManager.prototype.setSize = function (event)
+	{
+	var property=event.target.getAttribute('id').replace('-button','');
+	var element=document.getElementById(property);
+	var value = this.openPropertyDialog("chrome://bbcomposer/content/style/size.xul", element.value)
+	if(value)
+		element.value = value;
+	}
+
+StyleManager.prototype.setSizes = function (event)
+	{
+	var property=event.target.getAttribute('id').replace('-button','');
+	var size = this.openPropertyDialog("chrome://bbcomposer/content/style/size.xul", "");
 	if(size)
 		{
-		document.getElementById(size_type + "-left").value = size;
-		document.getElementById(size_type + "-right").value = size;
-		document.getElementById(size_type + "-top").value = size;
-		document.getElementById(size_type + "-bottom").value = size;
+		document.getElementById(property + "-left").value = size;
+		document.getElementById(property + "-right").value = size;
+		document.getElementById(property + "-top").value = size;
+		document.getElementById(property + "-bottom").value = size;
 		}
 	}
 
-function style_init()
+StyleManager.prototype.unLoad = function ()
 	{
-	document.removeEventListener('load', style_init, false);
-	if(window.parent.myBBComposerManager.toggleSidebar('css', true))
-		{
-		window.parent.myBBComposerManager.focusedBBComposer.getElementStyle(document.getElementById('bbcomposer-lockon-menu').value);
-		document.addEventListener('unload', style_uninit, false);
-		}
-	}
-
-function style_uninit()
-	{
-	document.removeEventListener('unload', style_uninit, false);
+	document.removeEventListener('unload', this.unLoadHandler, false);
 	window.parent.myBBComposerManager.toggleSidebar('css',false);
 	}
 
-window.addEventListener('load', style_init, false);
+new StyleManager();
